f4e7dab3f90adcac3ebfee162b4d5180e6cfb6fa
John Melody Me <johnmelodyme@yandex.com>
2023-01-31 18:22:33 +0800

    Initial Commit

---
+++ Cryato/CryatoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryatoApp.swift
+//  Cryato
+//
+//  Created by John Melody Me on 31/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CryatoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
