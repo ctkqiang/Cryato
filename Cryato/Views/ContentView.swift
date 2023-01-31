5790671417231df2255bffc60a49797518725ec5
John Melody Me <johnmelodyme@yandex.com>
2023-01-31 23:36:49 +0800

    Completed preferenceView contents and added donation page (Deep-linking to Binance)

--- Cryato/Views/ContentView.swift
+++ Cryato/Views/ContentView.swift
@@ -12,27 +12,33 @@ struct ContentView: View {
     @State private var selectedId = 0x0
     @State private var options = ["Sell", "Buy"]
     
-    private var navigationTitle = "I want to"
+    private var navigationTitle = "Cryato"
+    private var pickerTitle = "I want to"
     
     var body: some View {
         NavigationView {
             VStack {
                 Form {
-                    Picker(selection: $selectedId, label: Text("")) {
-                        Text(options[0]).tag(0x0)
-                        Text(options[1]).tag(0x1)
-                    }.pickerStyle(SegmentedPickerStyle()).listRowSeparator(.hidden, edges: [.bottom])
-                    
-                    
-                    if (self.selectedId == 0x0) {
-                        SellView()
-                    } else {
-                        BuyView()
+                    Section(header: Text(pickerTitle).foregroundColor(Color.black).bold().fixedSize()) {
+                        Picker(selection: $selectedId, label: Text("")) {
+                            Text(options[0]).tag(0x0)
+                            Text(options[1]).tag(0x1)
+                        }.pickerStyle(SegmentedPickerStyle()).listRowSeparator(
+                            .hidden,
+                            edges: [.bottom]
+                        )
+                        
+                        if (self.selectedId == 0x0) {
+                            SellView()
+                        } else {
+                            BuyView()
+                        }
                     }
                 }
             }.navigationTitle(navigationTitle).toolbar {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     NavigationLink {
+                        /** Navigate to app preference page */
                         PreferenceView()
                     } label: {
                         Image(systemName: "gearshape").foregroundColor(Color.black)
--- Cryato/Views/PreferenceView.swift
+++ Cryato/Views/PreferenceView.swift
@@ -8,8 +8,56 @@
 import SwiftUI
 
 struct PreferenceView: View {
+    @Environment(\.openURL) private var openURL
+    @State private var isPresentingAlert: Bool = false
+    @State private var selectedItem: String = ""
+    private var binanceUrl: String = "https://s.binance.com/8ER0kn3r"
+    private var preferenceItems: [String] = ["Who created this app?", "Support this app", "Version"]
+    
+    func displayAlert() -> Alert {
+        if (self.selectedItem == self.preferenceItems[1]) {
+            self.openUrl(url: self.binanceUrl)
+        }
+        
+        switch self.selectedItem {
+        case self.preferenceItems[0]:
+            return Alert(
+                title: Text(self.selectedItem),
+                message: Text(
+                    "This application is developed by \nJohn Melody Me"
+                ),
+                dismissButton: .default(Text("Okay"))
+            )
+        case self.preferenceItems[2]:
+            return Alert(
+                title: Text(self.selectedItem),
+                message: Text("You are running on Verions 1.0.0"),
+                dismissButton: .default(Text("Okay"))
+            )
+        default:
+            return Alert(title: Text(""), message: Text("Hi 🤗"))
+        }
+    }
+    
+    func openUrl(url :String) -> Void {
+        if (self.selectedItem == self.preferenceItems[1]) {
+            openURL(URL(string: url)!)
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(self.preferenceItems, id: \.self ) {
+                items in Button {
+                    self.isPresentingAlert = true
+                    self.selectedItem = items
+                } label: {
+                    Text(items).foregroundColor(Color.black)
+                }.alert(isPresented: self.$isPresentingAlert, content: {
+                    self.displayAlert()
+                }).fixedSize()
+            }
+        }
     }
 }
 
