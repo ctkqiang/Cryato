8a30f22abe855e8c18390fda594133fc0b73a3df
John Melody Me <johnmelodyme@icloud.com>
2023-02-13 04:54:58 +0800

    #4 added Binance integration

---
+++ Cryato/Endpoints/Binance.swift
@@ -0,0 +1,14 @@
+//
+//  Binance.swift
+//  Cryato
+//
+//  Created by John Melody Me on 13/02/2023.
+//
+
+struct Binance {
+    
+    public var apiEndpoints :String = "https://api.binance.com"
+    
+    /** Get Funding Account */
+    public var fundingAccount :String = "/sapi/v1/asset/get-funding-asset"
+}
---
+++ Cryato/Models/BinanceFundingAccount.swift
@@ -0,0 +1,12 @@
+//
+//  BinanceFundingAccount.swift
+//  Cryato
+//
+//  Created by John Melody Me on 13/02/2023.
+//
+
+struct BinanceFundingAccount {
+    public var asset :String
+    public var free :String
+    public var btcValuation :String
+}
