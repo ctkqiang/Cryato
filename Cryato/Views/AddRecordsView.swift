23f3873f1cba743cfd862d99ccc10d642f91c1de
John Melody Me <johnmelodyme@icloud.com>
2023-02-18 13:23:20 +0800

    #2 readjustment for the exception handler

--- Cryato/Views/AddRecordsView.swift
+++ Cryato/Views/AddRecordsView.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
+#if canImport(AlertToast)
+import AlertToast
+#endif
+
 struct AddRecordsView: View {
+    @Environment(\.presentationMode) private var mode: Binding<PresentationMode>
+    
     @State private var dateTime :String = ""
     @State private var unitPurchased :String = ""
     @State private var originalPrice :String = ""
     @State private var sellingPrice :String = ""
     @State private var selectedCrypto :String = CryptoSelector.cryptoCurrenciesChoices[0]
+    @State private var showAlertNil :Bool = false
+    @State private var showAlertSpec :Bool = false
     
     private var cryptoPair :[String] = CryptoSelector.cryptoCurrenciesChoices
     
@@ -21,8 +29,12 @@ struct AddRecordsView: View {
     }
     
     private func saveIntoDatabase() throws -> Void {
+        if try! self.unitPurchased.isContainSpecialChars() {
+            self.showAlertSpec = true
+        }
+        
         if !self.selectedCrypto.isEmpty &&
-        !self.unitPurchased.isEmpty &&
+            !self.unitPurchased.isEmpty &&
             !self.originalPrice.isEmpty &&
             !self.sellingPrice.isEmpty {
             try! DatabaseManager().addRecord(
@@ -32,6 +44,10 @@ struct AddRecordsView: View {
                 crypto: self.selectedCrypto,
                 earned: try! self.earned()
             )
+            self.mode.wrappedValue.dismiss()
+        } else {
+            self.showAlertNil = true
+            NSLog("The Record field are empty")
         }
     }
     
@@ -50,29 +66,46 @@ struct AddRecordsView: View {
                         .background(Color(.systemGray6))
                         .cornerRadius(5)
                         .disableAutocorrection(true)
+                        .keyboardType(.decimalPad)
                     
                     TextField("What was your selling price?", text: self.$sellingPrice)
                         .padding(10)
                         .background(Color(.systemGray6))
                         .cornerRadius(5)
                         .disableAutocorrection(true)
+                        .keyboardType(.decimalPad)
                 }
                 .navigationTitle("Add Record")
                 
                 Button {
                     try! self.saveIntoDatabase()
                 } label: {
-                    Text("SAVE TO DATABASE")
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .buttonStyle(.borderedProminent)
-                        .buttonBorderShape(.roundedRectangle)
-                        .tint(.green)
-                        .frame(height: 10)
-                        .controlSize(.large)
-                        .padding()
+                    Text("SAVE TO DATABASE").frame(minWidth: 0, maxWidth: .infinity)
                 }
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .buttonStyle(.borderedProminent)
+                .buttonBorderShape(.roundedRectangle)
+                .tint(.blue)
+                .frame(height: 60)
+                .controlSize(.large)
+                .padding()
             }
-        }.scrollDismissesKeyboard(.interactively)
+        }
+        .scrollDismissesKeyboard(.interactively)
+        .toast(isPresenting: self.$showAlertSpec, alert: {
+            AlertToast(
+                type: .error(.red),
+                title: "Warning",
+                subTitle: "The unit doesn't have special characters."
+            )
+        })
+        .toast(isPresenting: self.$showAlertNil, alert: {
+            AlertToast(
+                type: .error(.red),
+                title: "Warning",
+                subTitle:  "Please fill in all the required field!"
+            )
+        })
     }
 }
 
--- Cryato/Views/AddScamList.swift
+++ Cryato/Views/AddScamList.swift
@@ -17,6 +17,7 @@ import LogToDiscord
 
 struct AddScamList: View {
     @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.presentationMode) private var mode: Binding<PresentationMode>
     
     @State private var walletID: String = ""
     @State private var paymentMethod: String = ""
@@ -35,89 +36,89 @@ struct AddScamList: View {
     
     public var body: some View {
         NavigationView {
-            Form {
-                VStack(alignment: .leading) {
-                    Section(header: Text("")) {
-                        TextField(self.WalletIdPlaceholder, text: self.$walletID)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        TextField(self.PaymentMethodPlaceholder, text: self.$paymentMethod)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        TextField(self.CurrencyPlaceholder, text: self.$currency)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        TextField(self.PlatformPlaceholder, text: self.$platform)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        TextField(self.NamePlaceholder, text: self.$name)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        TextField(self.TechniquePlaceholder, text: self.$technique)
-                            .textFieldStyle(.roundedBorder)
-                            .font(Font.system(size: 12))
-                            .padding(5)
-                        
-                        NavigationLink {
-                            ScamView()
-                        } label: {
-                            Button {
-                                if self.currency.isEmpty ||
-                                    self.platform.isEmpty ||
-                                    self.name.isEmpty ||
-                                    self.technique.isEmpty {
-                                    self.showAlert = true
-                                } else {
-                                    
-                                    let data :[String: String] = [
-                                        "Name" : self.name,
-                                        "WalletID" : self.walletID,
-                                        "PaymentMethod" : self.paymentMethod,
-                                        "Currency" : self.currency,
-                                        "Platform" : self.platform,
-                                        "Techniques" : self.technique
-                                    ]
-                                    
-                                    if let discordChannel = Bundle.main.infoDictionary?["DISCORD_WEBHOOK"] as? String {
-                                        try! LogToDiscord(
-                                            appName: "Cryato APP",
-                                            webhookUrl: "https://\(discordChannel)"
-                                        ).send(message: "\(data)", level: Level.Debug, delay: 0x1)
-                                    }
-                                }
-                                
-                                hideKeyboard()
-                            } label: {
-                                Text("ADD TO DATABASE")
-                                    .frame(minWidth: 0, maxWidth: .infinity)
-                            }
+            VStack {
+                Form {
+                    VStack(alignment: .leading) {
+                        Section(header: Text("")) {
+                            TextField(self.WalletIdPlaceholder, text: self.$walletID)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
+                            
+                            TextField(self.PaymentMethodPlaceholder, text: self.$paymentMethod)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
+                            
+                            TextField(self.CurrencyPlaceholder, text: self.$currency)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
+                            
+                            TextField(self.PlatformPlaceholder, text: self.$platform)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
+                            
+                            TextField(self.NamePlaceholder, text: self.$name)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
+                            
+                            TextField(self.TechniquePlaceholder, text: self.$technique)
+                                .textFieldStyle(.roundedBorder)
+                                .font(Font.system(size: 12))
+                                .padding(5)
                         }
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .buttonStyle(.borderedProminent)
-                        .buttonBorderShape(.roundedRectangle)
-                        .tint(.blue)
-                        .frame(height: 60)
-                        .controlSize(.large)
-                        .padding()
                     }
+                    .toast(isPresenting: self.$showAlert, alert: {
+                        AlertToast(
+                            type: .error(.red),
+                            title: "Warning",
+                            subTitle: "Please fill in all the required field!\n(except for \"walletid\")"
+                        )
+                    })
                 }
-                .toast(isPresenting: self.$showAlert, alert: {
-                    AlertToast(
-                        type: .error(.red),
-                        title: "Warning",
-                        subTitle: "Please fill in all the required field!\n(except for \"walletid\")"
-                    )
-                })
+                Button {
+                    if self.currency.isEmpty ||
+                        self.platform.isEmpty ||
+                        self.name.isEmpty ||
+                        self.technique.isEmpty {
+                        self.showAlert = true
+                    } else {
+                        
+                        let data :[String: String] = [
+                            "Name" : self.name,
+                            "WalletID" : self.walletID,
+                            "PaymentMethod" : self.paymentMethod,
+                            "Currency" : self.currency,
+                            "Platform" : self.platform,
+                            "Techniques" : self.technique
+                        ]
+                        
+                        if let discordChannel = Bundle.main.infoDictionary?["DISCORD_WEBHOOK"] as? String {
+                            try! LogToDiscord(
+                                appName: "Cryato APP",
+                                webhookUrl: "https://\(discordChannel)"
+                            ).send(message: "\(data)", level: Level.Debug, delay: 0x1)
+                            
+                            self.mode.wrappedValue.dismiss()
+                        }
+                    }
+                    
+                    hideKeyboard()
+                } label: {
+                    Text("ADD TO DATABASE")
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                }
+            
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.roundedRectangle)
+            .tint(.blue)
+            .frame(height: 60)
+            .controlSize(.large)
+            .padding()
             }
         }
     }
